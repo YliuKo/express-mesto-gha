b9f813f8b509c463e475dcc6098fd85116d2e5ff
Юлия Колобкова <116802111+YliuKo@users.noreply.github.com>
2023-07-02 13:19:32 +0300

    Update app.js
    02.07.2023

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const auth = require('./middlewares/auth');
 const defaultError = require('./errors/defaultError');
 const { urlValidation } = require('./middlewares/validation');
 
-const { PORT = 3000 } = process.env;
+const { PORT = 3000, DB_URL = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;  
 const app = express();
 
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
